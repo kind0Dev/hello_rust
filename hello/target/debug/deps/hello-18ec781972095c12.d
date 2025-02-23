5f1fb3039ed8a993fd4edbbebe040058aeba0d4f
RAJI ABDUL <kind0devpro@gmail.com>
2025-02-23 21:57:13 +0100

    phase 1 inprogress

---
+++ hello/target/debug/deps/hello-18ec781972095c12.d
@@ -0,0 +1,5 @@
+/home/kind/Desktop/learning/rust/hello_rust/hello/target/debug/deps/hello-18ec781972095c12: src/main.rs
+
+/home/kind/Desktop/learning/rust/hello_rust/hello/target/debug/deps/hello-18ec781972095c12.d: src/main.rs
+
+src/main.rs:
---
+++ hello/target/debug/hello.d
@@ -0,0 +1,1 @@
+/home/kind/Desktop/learning/rust/hello_rust/hello/target/debug/hello: /home/kind/Desktop/learning/rust/hello_rust/hello/src/main.rs
